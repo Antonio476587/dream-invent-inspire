0a5a73567ce5dca8077d5dcf3db4c16023da180d
Antonio <fantonix.web@gmail.com>
2023-04-14 17:50:58 -0400

    styles(Views|wwwroot): Making the page look nicer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+barba.init({
+  transitions: [{
+    name: 'opacity-transition',
+    // leave(data) {
+    //   console.log(data);
+    //   return gsap.to(data.current.container, {
+    //     opacity: 0,
+    //   });
+    // },
+    leave(data) {
+      console.log(data);
+      return gsap.to(data.current.container, {
+        opacity: 0,
+        position: "absolute",
+        width: "100%",
+        top: 100
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0,
+        translateY: 100
+      });
+    }
+  }]
+});
